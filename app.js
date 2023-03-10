5f4e56e1eda47f7a2eb362f1f5c9981a47913196
Nebojsa Pavlovic <nebojsap82@gmail.com>
2023-03-10 20:16:16 +0100

    4th update before event loops

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// npm - global command, comes with node
-// command: npm --version
-
-// local dependency - use it only in this particular project 
-// command: npm i <packageName>
-
-// global dependency - use it in any project
-// command: npm install -g <packageName>
-// command: sudo npm install -g <packageName> (mac)
-
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// command: npm init (step by step, press enter to skip)
-// command: npm init -y (everything default)
 
 const {readFile} = require('fs');
 
